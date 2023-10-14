416b7014ef287c5b8c853e4c5bb1d2ea570ad136
Julian Dario Gonzalez <gonzalez_jd@live.com.ar>
2023-10-14 09:56:01 -0300

    footer

--- src/components/Header/Footer.jsx
+++ src/components/Header/Footer.jsx
@@ -1,18 +1,19 @@
 import Nav from "react-bootstrap/Nav";
 import "./footer.css";
+import img from "./logo-rocky-bikes.png";
 
 function BasicExample() {
   return (
     <div>
       <li className="footer">
-        <h4 className="home-footer">
-          <Nav.Link href="/">Rocky Bikes</Nav.Link>
-        </h4>
-        <h5>
-          <Nav.Link href="https://instagram.com">Instagram</Nav.Link>
+        <h5 className="logo-instagram">
+          <Nav.Link href="https://instagram.com">Seguinos en Instagram</Nav.Link>
         </h5>
-        <h5>
-          <Nav.Link href="https://facebook.com">Facebook</Nav.Link>{" "}
+        <h3 className="home-footer">
+        <Nav.Link href="/"> <img src={img}  width="30%" height="30%" alt="Logo-Bike" /> </Nav.Link>
+        </h3>
+        <h5 className="logo-facebook">
+          <Nav.Link href="https://facebook.com">Seguinos en Facebook</Nav.Link>
         </h5>
       </li>
     </div>

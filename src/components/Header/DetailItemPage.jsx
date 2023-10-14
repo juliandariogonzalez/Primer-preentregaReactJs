4863af2da9c06a74db4554503b8cb47bf3409983
Julian Dario Gonzalez <gonzalez_jd@live.com.ar>
2023-10-13 22:03:35 -0300

    api ML

--- src/components/Header/DetailItemPage.jsx
+++ src/components/Header/DetailItemPage.jsx
@@ -1,9 +1,27 @@
-const DetailItemPage = () => {
+import { useState, useEffect } from 'react';
+
+function DetailItem  (){
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.mercadolibre.com/sites/MLA/search?q=bicicletas')
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data.results.slice(0, 10)); 
+      })
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
-    <div>
-      <h1>Descripcion del Producto</h1>
+    <div className="Detalle">
+      <h1>Lista de Productos de Mercadolibre</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>{product.title}</li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
-export default DetailItemPage;
+export default DetailItem;
